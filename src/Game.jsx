471f75451f3c1906d9b81bb8f77c831f807705f4
pkro <pkrombach@web.de>
2020-01-05 09:07:11 +0100

    not perfect but functional. Done for now.

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import update from 'immutability-helper';
-import CardItem from './CardItem'
 import Board from './Board';
 import { createDeck, randomRotationStyle } from './helpers';
 import content from './data/cardContent.json';
 
-const numCards = 24;
+const numCards = 16;
 const strongRotationChance = 10;
 const strongRotationAdd = 8;
 const cardContent = content.emoticons;
@@ -16,7 +15,6 @@ export default class Game extends React.Component {
     super(props);
     this.state = {
       cards: createDeck(cardContent, numCards, strongRotationChance, strongRotationAdd),
-      lastCardTurned: new CardItem(0, '0', '0', false),
     };
   }
 
@@ -32,10 +30,45 @@ export default class Game extends React.Component {
   }
 
   gameLogic = () => {
-    console.log('running game logic');
     const { cards } = this.state;
+    // otherwise react seems to update immediately without calling setstate when changing a card property
+    // on the card array itself
+    // I guess this is what immutability-helper is supposed to solve, but i don't understand how
+    const myCards = JSON.parse(JSON.stringify(cards));
     const openCards = cards.filter(card => card.isFlipped);
-    // toDo: set openCards with same values to invisile
+    let action = 'flip';
+
+    if (openCards.length > 1) {
+      if (openCards[0].visibleContent === openCards[1].visibleContent) {
+        action = 'remove';
+      }
+
+      const openCardKeys = [openCards[0].key, openCards[1].key];
+      for (let i = 0; i < cards.length; i++) {
+        if (openCardKeys.includes(cards[i].key)) {
+          myCards[i].isFlipped = false;
+          if (action === 'remove') {
+            myCards[i].visible = false;
+          } else {
+            myCards[i].style = randomRotationStyle(strongRotationChance, strongRotationAdd);
+          }
+        }
+      }
+
+      setTimeout(
+        () => this.setState(state => update(state, { cards: { $set: myCards } })),
+        secondsToViewTurnedCard * 1000
+      );
+
+      if (myCards.filter(card => card.visible).length === 0) {
+        for (let i = 0; i < cards.length; i++) {
+          myCards[i].isFlipped = true;
+          myCards[i].visible = true;
+        }
+        this.setState(state => update(state, { cards: { $set: myCards } }));
+        alert("You won!");
+      }
+    }
   }
 
   render() {
